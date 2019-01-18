16018ef512ec9148ebabc2e3d30c052122c8efb4
eamiear <2639121731@qq.com>
2019-01-18 20:47:18 +0800

    add some message

--- npm-publish/npm-publish-demo/index.js
+++ npm-publish/npm-publish-demo/index.js
@@ -1,6 +1,8 @@
-exports.Utopa = {
+module.exports = {
+  extends: [
+    './lib/ua'
+  ].map(require.resolve),
   printMsg: function() {
     console.log('This is a demo for publishing npm package')
-  },
-  descript: ''
+  }
 }
---
+++ npm-publish/npm-publish-demo/lib/ua.js
@@ -0,0 +1,7 @@
+module.exports = {
+  browsers: {
+    ie: '',
+    chrome: ''
+  },
+  version: '1.0.0'
+}
